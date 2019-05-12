61ad59e04a0cc4cfb6fe433c4ec47140f7869de4
DanSoup <dansoup@hotmail.co.uk>
2019-05-12 14:16:07 +0100

    added navbar

--- src/components/homePage/header/header.js
+++ src/components/homePage/header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Subtitle from './subtitles/subtitles.js';
+import NavBar from './navBar/navBar.js'
 
 class Header extends React.Component {
 
@@ -9,13 +10,12 @@ class Header extends React.Component {
     }
 
     render() {        
-
+    
         return <header>
             <h1>DAN SOUP</h1>
             <Subtitle/>
-            <nav>
+            <NavBar/>
 
-            </nav>
         </header>
 
     }
---
+++ src/components/homePage/header/navBar/navBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NavBar extends React.Component {
+
+    state = {
+
+    };
+
+    render() {
+
+    return <React.Fragment>
+        NavBar
+    </React.Fragment>
+
+}
+
+}
+
+NavBar.propTypes = {
+
+};
+
+export default NavBar;
