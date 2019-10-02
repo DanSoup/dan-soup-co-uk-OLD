8b39c0415f961dbe0a0f6af69c8c94c5256ac85f
DanSoup <dansoup@hotmail.co.uk>
2019-10-02 14:08:51 +0100

    added random subtitles to the header

--- src/components/homePage/header/header.js
+++ src/components/homePage/header/header.js
@@ -5,10 +5,25 @@ import NavBar from './navBar/navBar.js';
 
 const Header = (props) => {
 
+  const subTitles = [
+    'Dot Co Dot UK',
+    'Bullet Bill\'s Best Friend',
+    'The Pinball Developer',
+    'Definitely Not Illegal',
+    'Run to the Helicopter'
+  ];
+
+  const RandomSubtitle = () => {
+
+    const chosenSubtitle = subTitles[Math.floor(Math.random() * subTitles.length)];
+
+    return <h2>{` - ${chosenSubtitle}`}</h2>
+  };
+
   return <>
     <header>
       <div className="mainTitle">
-        <h1>Dan Soup</h1><h2> - Dot Co Dot UK</h2>
+        <h1>Dan Soup</h1><RandomSubtitle/>
       </div>
     </header>
     <NavBar/>
