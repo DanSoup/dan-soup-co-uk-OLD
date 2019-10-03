2f49f3c8d55fa9204869b8660ed1fc4b635d32a2
DanSoup <dansoup@hotmail.co.uk>
2019-10-03 19:12:06 +0100

    added more formatting to the individual project component

--- src/components/homePage/content/projects/projects.js
+++ src/components/homePage/content/projects/projects.js
@@ -12,7 +12,12 @@ const Projects = (props) => {
 
   const ProjectPreview = () => {
     return <section>
-      <h1>Psoupocode</h1>
+      <img height="150" width="300" src="https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"></img>
+      <div>
+        <h1>Psoupocode</h1>
+        <h2>Published 10/2019</h2>
+        <p>A website where I publish coding tutorials. The aim is to be friendly to people completely new to the subject and to not assume prior knowledge without providing a tutorial for it.</p>
+      </div>
     </section>
   };
 
