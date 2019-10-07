47bf6c55f41a910a0f28b85ff6c673e4eb7fd82c
DanSoup <dansoup@hotmail.co.uk>
2019-10-07 14:21:04 +0100

    made the project previews smaller and without descriptions

--- src/components/homePage/content/projects/projects.js
+++ src/components/homePage/content/projects/projects.js
@@ -12,27 +12,19 @@ if (process.env.NODE_ENV === 'development') {
 
 const Projects = (props) => {
 
-  const ProjectPreview = () => {
+  const ProjectPreview = ({project}) => {
     return <section>
-      <img height="150" width="300" src="https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"></img>
-      <div>
-        <h1>Psoupocode</h1>
-        <h2>Published 10/2019</h2>
-        <p>A website where I publish coding tutorials. The aim is to be friendly to people completely new to the subject and to not assume prior knowledge without providing a tutorial for it.</p>
-      </div>
+      <img height="150" width="300" src={project.previewImg}></img>
+      <div className="spacer"></div>
+      <h1>{project.title}</h1>
+      <div className="spacer"></div>
+      <h2>Published 10/2019</h2>
     </section>
   };
 
   return <div className="projects">
     {projects.map(project => {
-      return <section>
-        <img height="150" width="300" src="https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg"></img>
-        <div>
-          <h1>{project.title}</h1>
-          <h2>Published 10/2019</h2>
-          <p>A website where I publish coding tutorials. The aim is to be friendly to people completely new to the subject and to not assume prior knowledge without providing a tutorial for it.</p>
-        </div>
-      </section>
+      return <ProjectPreview project={project}></ProjectPreview>
     })}
   </div>
 
