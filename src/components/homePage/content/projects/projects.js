371d1f4590e29dd2e9892bab703f904c6fd0894a
DanSoup <dansoup@hotmail.co.uk>
2019-10-12 07:39:23 +0100

    project previews now link to their project page

--- src/components/homePage/content/projects/projects.js
+++ src/components/homePage/content/projects/projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './projects.css';
 
@@ -13,13 +14,19 @@ if (process.env.NODE_ENV === 'development') {
 const Projects = (props) => {
 
   const ProjectPreview = ({project}) => {
-    return <section>
-      <img height="150" width="300" src={project.previewImg}></img>
-      <div className="spacer"></div>
-      <h1>{project.title}</h1>
-      <div className="spacer"></div>
-      <h2>Published 10/2019</h2>
-    </section>
+    return (
+      <section>
+        <Link to={`/projects/${project.title}`}>
+          <div>
+            <img height="150" width="300" src={project.previewImg}></img>
+            <div className="spacer"></div>
+            <h1>{project.title}</h1>
+            <div className="spacer"></div>
+            <h2>Published 10/2019</h2>
+          </div>
+        </Link>
+      </section>
+    )
   };
 
   return <div className="projects">
