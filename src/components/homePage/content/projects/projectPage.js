7495fc89634534ceca60c3594bf9fa42650559e6
DanSoup <dansoup@hotmail.co.uk>
2019-11-01 17:49:25 +0000

    project page now displays basic project information

--- src/components/homePage/content/projects/projectPage.js
+++ src/components/homePage/content/projects/projectPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './projectPage.css';
 
 let projects = [];
 
@@ -13,12 +14,17 @@ const ProjectPage = (props) => {
 
   const project = projects.find(project => project.title === props.location.pathname.match(/\/projects\/(.+)/)[1])
 
-  return <React.Fragment>
-    {project.title}
-    {project.published}
-    {project.link}
-
-  </React.Fragment>
+  return <section className="project-page">
+    <div>
+      <img height="150" width="300" src={project.previewImg}></img>
+      <div>
+        <h1>{project.title}</h1>
+        <h2>Published: {project.published}</h2>
+        <h2>Link: {project.link}</h2>
+      </div>
+    </div>
+    <p>{project.description}</p>
+  </section>
 
 }
 
