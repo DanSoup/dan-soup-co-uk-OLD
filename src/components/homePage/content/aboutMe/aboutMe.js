1e3f6055db1e2634b9432df1974eadfb662a7d80
DanSoup <dansoup@hotmail.co.uk>
2019-11-05 16:33:54 +0000

    gave aboutMe its own CSS file

--- src/components/homePage/content/aboutMe/aboutMe.js
+++ src/components/homePage/content/aboutMe/aboutMe.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './aboutMe.css';
 
 const AboutMe = (props) => {
 
   return <>
-    <section>
+    <section className="about-me">
       <p>My name's Daniel but I'm better known by my nickname Dan Soup. I'm a full stack developer with a background in mathematics and esports. I'm also a massive nerd.</p>
       <br/><br/>
       <p>After getting a mathematics degree and working for a few years in the leisure and retail sectors I decided to take a big step and do an intensive programming course at Northcoders. I got my first developer job a week after graduating and I have been working as a developer ever since.</p>
