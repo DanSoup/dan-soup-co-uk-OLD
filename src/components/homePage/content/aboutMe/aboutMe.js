7a1bb094321c3576872ce68fc634b8e3e1183e0f
DanSoup <dansoup@hotmail.co.uk>
2019-11-05 16:26:23 +0000

    removed unnecessary parts of the about me page

--- src/components/homePage/content/aboutMe/aboutMe.js
+++ src/components/homePage/content/aboutMe/aboutMe.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 
 const AboutMe = (props) => {
 
-  const aboutMeText = [
-    "My name's Daniel but I'm better known by my nickname Dan Soup. I'm a full-stack developer with a background in mathematics and esports. This is my story so far..."
-  ]
-
   return <>
     <section>
       <p>My name's Daniel but I'm better known by my nickname Dan Soup. I'm a full stack developer with a background in mathematics and esports. I'm also a massive nerd.</p>
