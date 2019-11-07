5a99f0cd76fe1e888ce32c5d326acb6d9b4379e9
DanSoup <dansoup@hotmail.co.uk>
2019-11-07 18:59:15 +0000

    Setting up the three columns for the home page

--- src/components/homePage/content/home/home.js
+++ src/components/homePage/content/home/home.js
@@ -6,8 +6,17 @@ import Projects from '../projects/projects.js';
 const Home = (props) => {
 
   return <div className="home-page">
-    <h1>Home Page Under Construction</h1>
-    <Projects/>
+    <section>
+      <h1>Latest Post</h1>
+      <p>The latest blog post I made will be here. BLAH BLAH BLAH BLAH BLAH Lfesof fes g grg rthres ghgtsht tsh thsh thtjuj hths ths hsr htrsshtrht shtrht shtthrsh rsh tshsrth ts rh tsrhh tsr ht h trs ht rs g</p>
+    </section>
+    <section>
+      <h1>Project Updates</h1>
+    </section>
+    <section>
+      <h1>About Me</h1>
+      <p>I'm Dan Soup. I'm a fullstack developer, mathematician, artist, gamer and former esports tournament organizer and streamer. I started coding in Visual Basic at university and continued to use it for various projects, mainly for collating esports data for the competitive Super Smash Bros. Melee community. I decided to take the Northcoders programming course and pursue a career as a developer.</p>
+    </section>
   </div>
 
 }
